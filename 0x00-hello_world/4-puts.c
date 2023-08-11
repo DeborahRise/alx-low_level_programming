1ea993728c4632ffc89999490eb697ed70f42f30
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-10 17:29:35 -0700

    out PUTS and 1st time with betty. love her already

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - using the put function to display ouput
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
