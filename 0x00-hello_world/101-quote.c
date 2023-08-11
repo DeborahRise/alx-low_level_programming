535fbe5e556ac1ea272a5bfe3039f33d509dbd64
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-10 19:49:29 -0700

    quote with errors

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - understanding the standard error and standard output functions.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
+
