e590999e1eea53dcf2c605bf9d0268e630eaff8e
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-11 19:21:42 -0700

    no e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase except q and e
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+if (c != 'e' && c != 'q')
+putchar(c);
+putchar('\n');
+return (0);
+}
