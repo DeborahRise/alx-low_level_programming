af9b78b42601bd067153cac9d36415fed9030e45
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-11 19:48:49 -0700

    base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits base 10
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
