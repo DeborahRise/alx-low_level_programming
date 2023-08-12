f8fb3a7ed7aeca949b2a5ad38cb15fb91c22bef2
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-11 19:38:49 -0700

    okay

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints lowercase except q and e
+ * main - prints single digits
  * Return: 0
  */
 int main(void)
@@ -9,6 +9,7 @@ int main(void)
 int i;
 
 for (i = 0; i <= 9; i++)
-printf("%d\n", i);
+printf("%d", i);
+putchar('\n');
 return (0);
 }
