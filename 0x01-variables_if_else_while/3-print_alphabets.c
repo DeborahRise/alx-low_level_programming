20b38f5bcf3f455cb5d91d503eb572b44bc107d7
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-11 19:12:14 -0700

    upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,13 @@
  */
 int main(void)
 {
-char a[] = "abcdefghijklmnopqrstuvwxyz";
-char A[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int c = 0;
-int d = c;
+int c;
 
-for (c = 0; c < 26; c++)
-{
-	d = c;
-	while (d = c)
-		putchar(a[d]);
-	d++;
-}
-putchar(A[c]);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
 putchar('\n');
+
 return (0);
 }
