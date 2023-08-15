409decaeb23e63843a6c5976d568a4ee30809180
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-15 12:49:41 -0700

    determines lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
