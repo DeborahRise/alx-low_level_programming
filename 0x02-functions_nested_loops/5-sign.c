d76d3eb92ee719b932b00852fc2bc8455a2241a9
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-15 13:32:44 -0700

    checks sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the sign of a number
+ * @n: parameter to be checked
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
