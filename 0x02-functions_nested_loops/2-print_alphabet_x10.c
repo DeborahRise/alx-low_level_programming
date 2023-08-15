796c63275a9be41c252d10b401272329ea415745
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-15 12:21:48 -0700

    print lowercase a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 /* _putchar prototype*/
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
