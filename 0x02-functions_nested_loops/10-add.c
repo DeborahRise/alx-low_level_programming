2ab9b89abac543e532ef40f4bcf0e6270c375eaa
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-15 17:47:49 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add 2 numbers
+ * @a: parameter 1
+ * @b: parameter to be checked
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int c = (a + b);
+
+	return (c);
+}
