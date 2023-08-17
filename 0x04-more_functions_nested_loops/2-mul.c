1b6e832a0001b17106b904927550e26bf733b2f0
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-17 12:37:55 -0700

    product of numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - to get the products of numbers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
