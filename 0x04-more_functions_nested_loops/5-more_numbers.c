b1b9da913cdb380345df799430a4a7b4c5691b0a
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-17 13:22:18 -0700

    print 0 to 14 with putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int d;
+
+	for (d = 0; d <= 14; d++)
+	{
+		if (d > 9)
+		{
+			_putchar(d / 10 + '0');
+		}
+		_putchar(d % 10 + '0');
+	}
+	_putchar('\n');
+}
