62a216f20f2fbddfb12ad4195aeded148e2436ac
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-17 12:58:45 -0700

    0 to 9, n0t 2 &4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 not 2, 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
