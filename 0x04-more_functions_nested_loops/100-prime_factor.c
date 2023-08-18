865fe196bb1e9f2023e3b940b0fb405b8cbfefc1
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-17 19:11:52 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	long int n = 612852475143;
-	long int i;
+	unsigned long n = 612852475143;
+	unsigned int i;
 
-	for (i = 2; i <= 612852475143; i++)
+	for (i = 2; i <= n; i++)
 	{
 		while (n % i == 0)
 		{
 			if (n == i)
 			{
-				printf("%ld is the largest prime factor\n", i);
+				printf("%lu is the largest prime factor\n", n);
 			}
 			n = (n / i);
 		}
