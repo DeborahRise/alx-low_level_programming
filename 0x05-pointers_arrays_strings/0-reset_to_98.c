980a9bdc9873125ef303daec9dd8457fd3492fc1
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-21 12:14:03 -0700

    1st pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets n to 98
+ * @*n: parameter pointer
+ * @n: parameter variable
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x05. C - Pointers, arrays and strings
+### C - Arrays
+### C - Pointers
+### C - Strings
+### Memory Layout
+
