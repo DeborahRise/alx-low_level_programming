80e25079340446ffc2a73a5dee82052fe2239ee0
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-21 16:51:23 -0700

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,11 @@
 
 void puts2(char *str)
 {
-	int i = 0;
-	int n = strlen(str);
+	int i;
 
-	while (i <= n)
-	{
-		_putchar(str[i]);
-		i = (i + 2);
-	}
+		for (i = 0; str[i] != '\0'; i += 2)
+		{
+			_putchar(str[i]);
+		}
 	_putchar('\n');
 }
