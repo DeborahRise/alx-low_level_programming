8a58f23a0848b99eaaa044060cc0e0c89e4b4f64
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-21 14:03:04 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string passed
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x = strlen(s);
+	int y = (x - 1);
+
+	while (y >= 0)
+	{
+		_putchar(s[y]);
+		y++;
+	}
+}
