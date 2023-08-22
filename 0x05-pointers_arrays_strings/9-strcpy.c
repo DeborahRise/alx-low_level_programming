9de4646505dcd44c6303340b1d89edabd4931074
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-21 18:57:32 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy - copies 2 strings
+ * @dest: first pointer
+ * @src: second parameter
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+		return (dest);
+}
