d1c0127f7867907f08cd2484e0644ef5a3b18db8
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-24 10:05:30 -0700

    we did it! Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @s: parameter array
+ * Return: 0
+ */
+
+char *rot13(char *s)
+{
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int length = strlen(s);
+	int i, j;
+
+	for (i = 0; i <= length; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+			}
+		}
+	}
+	return (s);
+}
