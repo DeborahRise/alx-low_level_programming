97a5661516f6aa68efa7679bf6a9beb63ea87956
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-24 11:05:59 -0700

    i dont ghet it

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,13 +13,13 @@ char *rot13(char *s)
 	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; ++i)
 	{
-		for (j = 0; alpha[j] != '\0'; j++)
+		for (j = 0; alpha[j] != '\0'; ++j)
 		{
 			if (s[i] == alpha[j])
 			{
-				s[i] = rot[j];
+				*(s + i) = *(rot + j);
 			}
 		}
 	}
