56b74b5dfb6a1380c145eb5be2e0da0b94c19ab6
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-24 18:51:21 -0700

    please work

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -18,22 +18,22 @@ void print_number(int n)
 		n = -n;
 		_putchar('-');
 	}
-	if (n >= 0 && n < 10)
+	while (n > 9)
 	{
-		_putchar(n + '0');
-	}
-	else
-	{
-		while (n > 9)
+		if (n >= 0 && n < 10)
+		{
+			_putchar(n + '0');
+		}
+		else
 		{
 			n = (n / 10);
 			count = count * 10;
 		}
-		while (count >= 1)
-		{
-			a = ((x / count) % 10);
-			_putchar(a + '0');
-			count = count / 10;
-		}
+	}
+	while (count >= 1)
+	{
+		a = ((x / count) % 10);
+		_putchar(a + '0');
+		count = count / 10;
 	}
 }
