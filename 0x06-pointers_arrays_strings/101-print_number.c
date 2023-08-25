24ad8c112549b719aea688e2bc80e274a554fc94
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-24 17:17:49 -0700

    prints integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,32 +9,31 @@
 
 void print_number(int n)
 {
-	int i = n;
 	int a;
 	int count = 1;
 	int x = n;
 
 	if (n < 0)
 	{
-		x = -(n);
-		i = -(i);
-		putchar('-');
+		x = -n;
+		n = -n;
+		_putchar('-');
 	}
-	if (i >= 0 && i < 10)
+	if (n >= 0 && n < 10)
 	{
-		putchar(i + '0');
+		_putchar(n + '0');
 	}
 	else
 	{
-		while (i >= 10)
+		while (n > 9)
 		{
-		i = i / 10;
-		count = count * 10;
+			n = (n / 10);
+			count = count * 10;
 		}
 		while (count >= 1)
 		{
 			a = ((x / count) % 10);
-			putchar(a + '0');
+			_putchar(a + '0');
 			count = count / 10;
 		}
 	}
