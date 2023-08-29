468a4b012e52470773eba77fe38f5568638d83ac
DeborahRise <debie.rise.bookings@gmail.com>
2023-08-28 17:35:07 -0700

    diag sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,8 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1, sum2 = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for  (i = 0; i < size; i++)
 	{
@@ -22,5 +23,5 @@ void print_diagsums(int *a, int size)
 	{
 		sum2 = sum2 + a[i * size - i];
 	}
-	printf("%d, %d", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
