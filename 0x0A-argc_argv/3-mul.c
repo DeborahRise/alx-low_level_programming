1d87a374b4a0351e051967079864b5b8ad281961
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-01 15:35:36 -0700

    cmd line multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: number of cmd line arguments
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, x;
+	int prod = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+		x = atoi(argv[i]);
+		prod = prod * x;
+		}
+	printf("%d\n", prod);
+	}
+
+	return  (0);
+}
