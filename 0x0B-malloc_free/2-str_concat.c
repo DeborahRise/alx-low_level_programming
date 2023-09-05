9f6f31fa95641bd8442f7528e8837d28642c3744
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-05 07:03:53 -0700

    malloc error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	size = len1 + len2;
@@ -25,8 +29,12 @@ char *str_concat(char *s1, char *s2)
 	ptr = (char *) malloc(sizeof(char) * size + 1);
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		ptr[i] = s1[i];
+	}
 	for (i = 0; s2[i] != '\0'; i++)
+	{
 		ptr[len1 + i] = s2[i];
+	}
 	return (ptr);
 }
