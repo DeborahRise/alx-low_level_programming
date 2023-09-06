d9f5e0c2d720dd9259739ee93b89124dcf7e783c
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-05 21:48:24 -0700

    ok

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,7 +15,7 @@ int word_count(char *str)
 	int counter = 0;
 
 	len = strlen(str);
-	for (i = 0; for i <= len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
@@ -34,5 +34,45 @@ int word_count(char *str)
 
 char **strtow(char *str)
 {
-	if 	
+	int row, i, s, x, j;
+	int position = 0;
+	char **arr;
+
+	if (str == NULL || str == '\0')
+	{
+		return (NULL);
+	}
+	row = word_count(str);
+	arr = (char **) malloc(row * sizeof(char *) + 1);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < row; x++)
+	{
+		s = strlen(str[x]);
+		arr[x] = (char *) malloc(s * sizeof(char) + 1);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < row; i++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < s; j++)
+		{
+			arr[position] = arr[x][j];
+			position++;
+		}
+	}
+	arr[position] = '\0';
+	return (arr);
+	for (i = 0; i < row; i++)
+	{
+		free(arr[i]);
+	}
+	free(arr);
+
 }
