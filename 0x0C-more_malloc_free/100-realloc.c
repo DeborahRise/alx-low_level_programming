10c428d47e4ac66c3e95e9434fa7542c80c5916c
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-07 19:41:12 -0700

    realloc oo

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (new_ptr);
 	}
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
