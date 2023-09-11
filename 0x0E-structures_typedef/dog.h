38360e4b1a9a02fc790a88a12a2459e0aa021ed8
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-11 13:12:39 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@ int _putchar(char);
  *  @age: dog age
  *  @owner: dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
