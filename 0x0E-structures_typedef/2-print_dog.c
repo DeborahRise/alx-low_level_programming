b043557edb8ca47a10596577e7d5a6ed0618834c
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-11 13:03:55 -0700

    na wa o

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,29 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: nill\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-		if (d->age == 0)
-		{
-			printf("Age: \n");
-		}
-		else
-		{
-			printf("Age: %f\n", d->age);
-		}
-		if (d->owner == NULL)
-		{
-			printf("owner: nill\n");
-		}
-		else
-		{
-			printf("Owner: %s\n", d->owner);
-		}
+		printf("Name: %s\n", (d->name) ? (d->name) : "(nil)");
+		printf("Age: %f\n", (d->age) ? (d->age) : 0);
+		printf("Owner: %s\n", (d->owner) ? (d->owner) : "(nil)");
 	}
 }
