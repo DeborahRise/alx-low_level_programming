95c35b150938eaca33f20710fcb7efd1aa63215f
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-11 11:20:15 -0700

    woof

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,12 +13,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-	{
-		free(d);
-	}
-	else
+	if (d != NULL)
 	{
 	(*d).name = name;
 	(*d).age = age;
