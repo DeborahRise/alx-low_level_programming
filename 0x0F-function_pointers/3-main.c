eae774ad0855f74c29fffc95e2ffd1f71f133c19
DeborahRise <debie.rise.bookings@gmail.com>
2023-09-13 16:41:04 -0700

    moving to vagrant

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,5 @@
+#include "calc.h"
+
+/**
+ * main - main entry
+ * @argc; 
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+#include <stdio>
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
