0f2dd96f6b5e5076d7d5a1cb75a5f9883f8e036d
debie.rise.bookings@gmail.com <debie.rise.bookings@gmail.com>
2023-09-22 00:34:32 +0000

    it worked!

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * add_node_end - a function that adds a new node
+ * at the end of a list_t list.
+ * @head: head of node
+ * @str: value of elements
+ * Return: New node.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *transverse;
+
+	new = (list_t *) malloc(sizeof(list_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(str);
+
+	if (!*head)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		transverse = *head;
+		while (transverse->next)
+		{
+			transverse = transverse->next;
+		}
+		new->next = NULL;
+		transverse->next = new;
+	}
+	return (new);
+	free(new);
+}
