f434e926004c637bcd45d6e37da69ae318f302dd
debie.rise.bookings@gmail.com <debie.rise.bookings@gmail.com>
2023-09-26 02:04:44 +0000

    hm

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,12 +16,14 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	while ((*head)->next)
+	temp = *head;
+
+	while (temp->next)
 	{
-		temp = (*head)->next;
+		temp = temp->next;
 		free(*head);
 		*head = temp;
 	}
-	free(*head);
+
 	*head = NULL;
 }
