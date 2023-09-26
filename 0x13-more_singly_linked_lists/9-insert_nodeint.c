2c35aa6b61f2361c2dd1072ff02586530d149539
debie.rise.bookings@gmail.com <debie.rise.bookings@gmail.com>
2023-09-26 15:45:53 +0000

    repaired the condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 
 	i = 0;
-	while (i < idx)
+	while (i < idx - 1)
 	{
 		temp = temp->next;
 		if (temp == NULL)
