a59232924deafe590386d58f4da0148597a908a3
debie.rise.bookings@gmail.com <debie.rise.bookings@gmail.com>
2023-09-27 02:59:51 +0000

    attempt

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,24 +8,48 @@
 
 size_t print_listint_safe(const listint_t *head);
 size_t print_listint_safe(const listint_t *head)
-{ size_t nodes = 0;
-	const listint_t *current = head;
+{
+	size_t nodes = 0;
+	const listint_t *slow_ptr = head;
+	const listint_t *fast_ptr = head;
 
 	if (!head)
 		exit(98);
 
-	while (current)
+	while (fast_ptr && fast_ptr->next)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 		nodes++;
-		current = current->next;
 
-		if (current && current <= head)
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+
+		if (slow_ptr == fast_ptr)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
+			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 			break;
 		}
 	}
+	if (slow_ptr == fast_ptr)
+	{
+		slow_ptr = head;
+		while (slow_ptr != fast_ptr)
+		{
+			printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			nodes++;
+			slow_ptr = slow_ptr->next;
+			fast_ptr = fast_ptr->next;
+		}
+	}
+	else
+	{
+		while (slow_ptr)
+		{
+			printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			nodes++;
+			slow_ptr = slow_ptr->next;
+		}
+	}
 
-	return nodes;
+	return (nodes);
 }
