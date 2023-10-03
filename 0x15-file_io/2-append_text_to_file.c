ca671655c79be154cc9d102f1896666a89cfa22c
DeborahRise <debie.rise.bookings@gmail.com>
2023-10-03 04:59:57 +0000

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,26 +11,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_desc, write_txt, len;
-
-	if (!filename)
-		return (-1);
-
-	file_desc = open(filename, O_RDWR | O_APPEND);
-	if (!file_desc)
-		return (-1);
-
-	if (text_content)
 	{
-		len = strlen(text_content);
+		int file;
+		int final_out;
+		int x;
 
-		write_txt = write(file_desc, text_content, len);
-		if (!write_txt)
-		{
-			close(file_desc);
+		file = open(filename, O_RDWR | O_APPEND);
+		if (filename == NULL)
 			return (-1);
-		}
+		if (file == -1)
+			return (-1);
+		if (text_content == NULL)
+			return (1);
+		while (*(text_content + x) != '\0')
+			x++;
+		final_out = write(file, text_content, x);
+		if (final_out == -1)
+			return (-1);
+		return (1);
 	}
-	close(file_desc);
-	return (1);
 }
