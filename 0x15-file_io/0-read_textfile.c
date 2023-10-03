2d553bdff08098fd2e85718fd13a7440fe0a52ef
DeborahRise <debie.rise.bookings@gmail.com>
2023-10-03 00:37:27 +0000

    read _text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/*
+ * read_textfile - a function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: filename passed
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_desc;
+	ssize_t read_txt, write_txt;
+	char *store;
+
+	if (!filename)
+		return (0);
+
+	store = malloc(sizeof(char) * letters);
+	if (!store)
+		return (0);
+
+	file_desc = open(filename, O_RDONLY);
+	if (file_desc == -1)
+	{
+		free(store);
+		return (0);
+	}
+
+	read_txt = read(file_desc, store, letters);
+	if (read_txt == -1)
+	{
+		free(store);
+		close(file_desc);
+		return (0);
+	}
+	close(file_desc);
+
+	write_txt = write(STDOUT_FILENO, store, read_txt);
+		if (write_txt == -1)
+		{
+			free(store);
+			return (0);
+		}
+	if (write_txt != read_txt)
+	{
+		return (0);
+	}
+	return (read_txt);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+# 0x15. C - File I/O
+
+### Write a function that reads a text file and prints it to the POSIX standard output.
+### Create a function that creates a file.
+### Write a function that appends text at the end of a file.
+### Write a program that copies the content of a file to another file.
+### Write a program that displays the information contained in the ELF header at the start of an ELF file.
