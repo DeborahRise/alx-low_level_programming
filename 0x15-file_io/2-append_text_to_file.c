b619ed85e3b49e21c4d8add8deb27bee8f86b8a0
DeborahRise <debie.rise.bookings@gmail.com>
2023-10-05 03:10:44 +0000

    O_WRONLY

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		int final_out;
 		int x;
 
-		file = open(filename, O_RDWR | O_APPEND);
+		file = open(filename, O_WRONLY | O_APPEND);
 		if (filename == NULL)
 			return (-1);
 		if (file == -1)
