81539067e508530f722367528318f9a5fc8f08be
DeborahRise <debie.rise.bookings@gmail.com>
2023-10-06 03:15:30 +0000

    fixed the issue

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,23 +11,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file_desc;
+	int count;
+	int read_text;
 
-		int file;
-		int final_out;
-		int x;
+	if (!filename)
+		return (-1);
 
-		file = open(filename, O_WRONLY | O_APPEND);
-		if (filename == NULL)
-			return (-1);
-		if (file == -1)
-			return (-1);
-		if (text_content == NULL)
-			return (1);
-		while (*(text_content + x) != '\0')
-			x++;
-		final_out = write(file, text_content, x);
-		if (final_out == -1)
-			return (-1);
-		return (1);
+	file_desc = open(filename, O_WRONLY | O_APPEND);
 
+	if (file_desc == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (count = 0; text_content[count]; count++)
+		read_text = write(file_desc, text_content, count);
+
+		if (read_text == -1)
+			return (-1);
+	}
+
+	close(file_desc);
+
+	return (1);
 }
