4dbb88c778d7a9bb8ec4b0893b1b86ed46032aec
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-13 17:09:03 +0000

    now

---
+++ 0x18-dynamic_libraries/100-ops.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * op_sub - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: sub
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: div
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - returns the sum of a and b
+ * @a: first value
+ * @b: second value
+ * Return: mod
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
