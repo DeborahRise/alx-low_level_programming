5b061e3f6f19e37ce95b1d3ca0a5c71e1356c491
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-14 00:39:10 +0000

    betty?

--- 0x18-dynamic_libraries/jackpot.c
+++ 0x18-dynamic_libraries/jackpot.c
@@ -1,8 +1,9 @@
 int jackpot(void)
 {
-	static int t[] = {8, 8, 7, 9, 23, 74};
- static int i = 0;
+	static int num[] = {8, 8, 7, 9, 23, 74};
+	static int i = 0;
 
- i = i > 5 ? 0 : i;
- return t[i++];
+	i = i > 5 ? 0 : i;
+
+	return (num[i++]);
 }
