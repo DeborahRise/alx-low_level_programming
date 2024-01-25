ebed9192b94d9175e691f63bb03ae1b50c728df2
DeborahRise <debie.rise.bookings@gmail.com>
2024-01-25 02:05:37 +0000

    task 5 vs 2

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
 
-    wd = len(grid[0])
-    ht = len(grid)
-    per = 0
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
 
-    for c in range(ht):
-        for h in range(wd):
-            if grid[c][h] == 0:
-                if (h > 0 and grid[c][h - 1] == 1):
-                    per += 1
-                if (c > 0 and grid[c - 1][h] == 1):
-                    per += 1
-                if (h < wd - 1 and grid[c][h + 1] == 1):
-                    per += 1
-                if (c < ht - 1 and grid[c + 1][h] == 1):
-                    per += 1
-    return per
+    return perimeter
+
