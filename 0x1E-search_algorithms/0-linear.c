5a3ef085d5085e5f7d428d40979dc49022dce681
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-11 17:37:00 +0100

    Task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,11 @@
 #include "search_algos.h"
-/*
- *
+/**
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located, -1 if value is not present
  */
 
 int linear_search(int *array, size_t size, int value)
