34ffaa4ee79c51873f4268d5384d400bc6898475
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-11 18:20:28 +0100

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,19 +11,19 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+	int i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (i = 0; i < (int)size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            printf("Found %d at index: %d\n", value, i);
-            return (i);
-        }
-    }
-    return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %d\n", value, i);
+			return (i);
+		}
+	}
+	return (-1);
 }
