71d06dc8f5a209a442555c9e1a6eea3b110aec8d
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-12 06:00:58 +0100

    106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -16,28 +16,44 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *current, *prev;
+    skiplist_t *temp = NULL, *stop = NULL;
 
     if (!list)
         return (NULL);
 
-    for (current = list; current; current = current->express)
+    temp = list;
+    while (temp && temp->express && temp->express->n < value)
     {
-        prev = current;
-        printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
-        if (current->n >= value)
-            break;
+        printf("Value checked at index [%lu] = [%i]\n",
+               temp->express->index, temp->express->n);
+        temp = temp->express;
     }
-    if (!current)
+    stop = temp;
+    while (stop && stop->next != stop->express)
+        stop = stop->next;
+    /* value potentially lies between two express nodes */
+    if (temp->express)
+    {
+        printf("Value checked at index [%lu] = [%i]\n",
+               temp->express->index, temp->express->n);
+        printf("Value found between indexes [%lu] and [%lu]\n",
+               temp->index, temp->express->index);
+    }
+    /* value is greater than last express node, potentially out of list */
+    else
+        printf("Value found between indexes [%lu] and [%lu]\n",
+               temp->index, stop->index);
+
+    while (temp != stop && temp->n < value)
+    {
+        printf("Value checked at index [%lu] = [%i]\n",
+               temp->index, temp->n);
+        temp = temp->next;
+    }
+    printf("Value checked at index [%lu] = [%i]\n",
+           temp->index, temp->n);
+
+    if (temp == stop)
         return (NULL);
-    printf("Value found between indexes [%ld] and [%ld]\n", prev->index, current->index);
-    for (; prev; prev = prev->next)
-    {
-        printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
-        if (prev->n == value)
-            return (prev);
-        if (prev->n > value)
-            return (NULL);
-    }
-    return (NULL);
+    return (temp);
 }
