9451921ab778c88a85d2e1a1b2666a48a61e8205
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-12 04:14:00 +0100

    100-jump

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,7 @@
 #include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value);
 
 int advanced_binary(int *array, size_t size, int value);
 #endif
